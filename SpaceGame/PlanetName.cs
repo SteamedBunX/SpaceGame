493c839ce7a40e19f072706613a133f3d14adb43
Konstantin Galkin <galkink@my.erau.edu>
2019-03-17 15:59:27 -0400

    Added Planet Name Generator class

---
+++ SpaceGame/PlanetName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace SpaceGame
+{
+    public class PlanetName
+    {
+        public string PlanetNameGenerator()
+        {
+
+            var name = new StringBuilder();
+            var random = new Random();
+
+            var planetLetterIndex = ((char)('a' + random.Next(0, 26))).ToString();
+            var planetNumberIndex = random.Next(100, 999).ToString();
+
+            name = name.Append(planetLetterIndex.ToUpper());
+            name = name.Append("-");
+            name = name.Append(planetNumberIndex);
+
+            return name.ToString();
+        }
+    }
+}
